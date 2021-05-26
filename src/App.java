72b3bb7ab33c2b518f31ed18a42acd883a7e8160
tien96ng <tien96ng@gmail.com>
2021-05-25 20:22:44 -0700

    Reading each matrix as individual ArrayList int items

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.lang.*;
+import java.time.temporal.Temporal;
 
 
 public class App {
@@ -13,14 +14,17 @@ public class App {
     Scanner input = null;
     PrintStream output = null;
 
-    ArrayList<String> temp = new ArrayList<String>();
+    ArrayList<Integer> masterList = new ArrayList<Integer>();
     String current;
-    int count = 0;
 
     String inFileName = "src/files/input/in3a.txt";
     String outFileName = "src/files/output/out3a.txt";
     boolean filesOk = false; // Indicates if the files are accessable. // other variables such as arrays, should also be declared here.
     boolean isMatrixLine = true;
+    String matrixValueString = "";
+    int[][] tempArr;
+    int x = 0;
+    int y = 0;
 
 
     try {
@@ -34,36 +38,47 @@ public class App {
       while(input.hasNextLine()) {
         current = input.nextLine();
         //System.out.println("LINE " + count + ": " + current);
-        
-        count++;
-        String arrNAme = "arr" + Integer.toString(count);
-
-        System.out.println(current.replaceAll("\\s+", ""));
-        if(isMatrixLine) {
-          String matrixValueString = current.replaceAll("\\s+", "");
-          int [][] tempArr;
-          isMatrixLine = false;
-        }
-
 
         if(current.length() == 0) {
-          System.out.println("BLANK LINE");
-          isMatrixLine = true;
-          continue;
+            isMatrixLine = true;
+            System.out.println("NEXT");
+            continue;
+        }
+        
+
+        if(isMatrixLine) {
+          matrixValueString = current.replaceAll("\\s+", "");
+          x = Character.getNumericValue(matrixValueString.charAt(0));
+          y = Character.getNumericValue(matrixValueString.charAt(1));
+          isMatrixLine = false;
         } else {
-          temp.add(current);
-          // System.out.println(current);
-          for(int i = 0; i <= current.split(" ").length; i++) {
-            System.out.println(x);
+          String[] currentArr = current.split(" ");
+          ArrayList<Integer> tempList = new ArrayList<Integer>();
+          int[] arr1 = new int[currentArr.length];
+          int spaceCount = 0;
+
+          for(int i = 0; i < currentArr.length; i++) {
+
+            if(currentArr[i] != "") {
+              int tempConvert = Integer.parseInt(currentArr[i]);
+              tempList.add(tempConvert);
+              arr1[i] = tempConvert;
+            } else {
+              spaceCount++;
+            }
           }
 
-        }
+          // System.out.println(tempList);
+          int[] arr2 = new int[arr1.length - spaceCount];
 
+          System.out.println("SPACES: " + spaceCount);
+          System.out.println(Arrays.toString(arr1));
+        }
       }
     }
 
     // System.out.println(temp);
-    String matrixKeys = temp.get(0).replaceAll("\\s+", "");
+    // String matrixKeys = temp.get(0).replaceAll("\\s+", "");
     // System.out.println(matrixKeys.charAt(0));
 
     
